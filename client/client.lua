1f1339f15b82bc3490670fae31322d7b485e4ca0
riversafe33 <riversafe33@outlook.com>
2025-11-03 08:48:43 +0100

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -415,6 +415,17 @@ local consumeAnimations = {
 	}
 }
 
+function eatstew()
+    local bowl = CreateObject("p_bowl04x_stew", GetEntityCoords(PlayerPedId()), true, true, false, false, true)
+    local spoon = CreateObject("p_spoon01x", GetEntityCoords(PlayerPedId()), true, true, false, false, true)
+    Citizen.InvokeNative(0x669655FFB29EF1A9, bowl, 0, "Stew_Fill", 1.0)
+    Citizen.InvokeNative(0xCAAF2BCCFEF37F77, bowl, 20)
+    Citizen.InvokeNative(0xCAAF2BCCFEF37F77, spoon, 82)
+    TaskItemInteraction_2(PlayerPedId(), 599184882, bowl, GetHashKey("p_bowl04x_stew_ph_l_hand"), -583731576, 1, 0, -1.0)
+    TaskItemInteraction_2(PlayerPedId(), 599184882, spoon, GetHashKey("p_spoon01x_ph_r_hand"), -583731576, 1, 0, -1.0)
+    Citizen.InvokeNative(0xB35370D5353995CB, PlayerPedId(), -583731576, 1.0)
+end
+
 local activeConsumeProp = nil
 
 local function cleanupConsumeProp()
@@ -452,6 +463,11 @@ local function playConsumeAnimation(spec)
 	elseif type(spec) == 'string' and spec ~= '' then
 		propName = spec
 	end
+
+	if animType == 'stew' then
+        eatstew()
+        return
+    end
 
 	local animDef = consumeAnimations[animType] or consumeAnimations.eat
 	local dict = animDef.dict
